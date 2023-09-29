1febaea22342d517fd6c14e91c9c7b4863cfa169
VelizhaninPavel <velizhaninpavel@gmail.com>
2023-09-29 22:57:53 +0500

    Task7

---
+++ Tyuiu.VelizhaninPI.Sprint1.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.VelizhaninPI.Sprint1.Task7.V10.Lib;
+
+namespace Tyuiu.VelizhaninPI.Sprint1.Task7.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            double x = 1;
+            var res = ds.Calculate(x);
+            var wait = -13.142;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.VelizhaninPI.Sprint1.Task7.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VelizhaninPI.Sprint1.Task7.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.VelizhaninPI.Sprint1.Task7.V10.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3701770f-e884-4412-9c7d-768f61f05654")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
